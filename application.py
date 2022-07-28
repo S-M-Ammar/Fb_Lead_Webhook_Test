2f2ce8b969be760a853670548ce3c8ecdcfbce7f
Syed Muhammad Ammar <syed.ammar.98sep@gmail.com>
2022-07-28 22:24:13 +0500

    test

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request
+application = Flask(__name__)
+
+@application.route("/")
+def home():
+    return ("Home Page")
+
+
+@application.route("/webhook",methods=['POST','GET'])
+def webhook():
+    print(request.form)
+    print("Got something from facebook")
+    return ("Got request")
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
